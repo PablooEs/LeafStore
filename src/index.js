0f2076b1fd8b25922f46c6d5dba667f6a8c41e46
PablooEs <pablo_j_es@hotmail.com>
2021-01-13 17:04:29 -0300

    Array content

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//CSS
+import './index.css'
+
+//Setup vars
+
+const plants = [
+{
+  img: "https://i.pinimg.com/736x/ca/b4/73/cab473942dab3958ea156067e072ca28.jpg",
+  name: 'Suculenta',
+  desc: 'Las plantas suculentas son aquellas en las que algún órgano está especializado en el almacenamiento de agua en cantidades mayores que las plantas sin esta adaptación.',
+  price: '178.53'
+},
+
+{
+  img: "https://www.bonsaiempire.es/great-bonsai-album/acer-bonsai-pall.jpg",
+  name: 'Bonsai',
+  desc: 'Es un bonsai.',
+  price: '433.29'
+},
+];
+
 function PlantList(){
   return(
-    <section>
-      <Plant/>
+    <section className='plantList'>
+      {plants.map((plant)=>{
+        return (<Plant plant = {plant}></Plant>);
+      })}
     </section>
   );
 }
 
-const Plant = () => {
+const Plant = (props) => {
+  const {img, name, desc, price} = props.plant;
   return( 
-  <article>
-    <Image/>
-    <h4>This is a plant</h4>
-    <p>descripcion</p>
+  <article className = "plant">
+    <img height = "200" src={img} alt=""/>
+    <h4>{name}</h4>
+    <p>{desc}</p>
+    <p>{price}</p>
   </article> 
   );
 }
-
-const Image = () => (<img height = "200" src="https://i.pinimg.com/736x/ca/b4/73/cab473942dab3958ea156067e072ca28.jpg" alt=""/> 
-);
-
 
 ReactDom.render(<PlantList/>, document.getElementById('root'));
