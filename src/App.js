4113e01fbdfa2abde63edea6ef8e77553aeabc7c
Pablo Escobar <pablo_j_es@hotmail.com>
2021-01-17 22:05:10 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Footer from './content/layout/footer';
 import Nav from './content/layout/nav';
 import Main from './content/main';
+import Cart from './content/cart';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 export default function App() {
   
   return (
     <React.Fragment>
+      <Router>
         <Nav/>
-        <Main/>
+        <Route path="/main" component={Main}/>
+        <Route path="/cart" component={Cart}/>
         <Footer/>
+      </Router>
     </React.Fragment>
   );
 }
--- src/content/main.js
+++ src/content/main.js
@@ -1,4 +1,3 @@
-import {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
